4a4472fe450434249a9799ebb73feab509743ca3
Genyus <anthonyfernandez16@gmail.com>
2014-10-29 14:47:23 +0100

    initial commit

---
+++ src/com/anthony/fernandez/nightly/MainActivity.java
@@ -0,0 +1,16 @@
+package com.anthony.fernandez.nightly;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		getSupportActionBar().setIcon(null);
+	}
+}

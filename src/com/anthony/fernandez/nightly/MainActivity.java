3015798c9610d91f30dccf87467f9a99309326e9
Genyus <anthonyfernandez16@gmail.com>
2014-10-30 23:51:32 +0100

    add viewpager, fragment, right panel with clock alarm, transaction UI changement

--- src/com/anthony/fernandez/nightly/MainActivity.java
+++ src/com/anthony/fernandez/nightly/MainActivity.java
@@ -1,19 +1,50 @@
 package com.anthony.fernandez.nightly;
 
+import java.util.List;
+import java.util.Vector;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.anthony.fernandez.nightly.adapter.InitPagerAdapter;
+import com.anthony.fernandez.nightly.fragment.LeftPanel;
+import com.anthony.fernandez.nightly.fragment.RightPanel;
+import com.viewpagerindicator.CirclePageIndicator;
 
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends SherlockFragmentActivity {
+
+	private PagerAdapter pagerAdapter;
+	public static ViewPager pager;
+	private List<Object> fragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 		getSupportActionBar().setCustomView(R.layout.action_bar_menu);
 		getSupportActionBar().setHomeButtonEnabled(true);
+
+		fragments = new Vector<Object>();
+
+		// Ajout des Fragments dans la liste
+		fragments.add(Fragment.instantiate(this,LeftPanel.class.getName()));
+		fragments.add(Fragment.instantiate(this,RightPanel.class.getName()));
+
+		this.pagerAdapter = new InitPagerAdapter(super.getSupportFragmentManager(), fragments);
+
+		pager = (ViewPager) findViewById(R.id.viewpager);
+		// Affectation de l'adapter au ViewPager
+		pager.setAdapter(this.pagerAdapter);
+
+		CirclePageIndicator indicator = (CirclePageIndicator)findViewById(R.id.indicator);
+		indicator.setFillColor(getResources().getColor(R.color.pink_circle));
+        indicator.setStrokeColor(getResources().getColor(android.R.color.white));
+		indicator.setViewPager(pager);
 	}
 }
---
+++ src/com/anthony/fernandez/nightly/fragment/LeftPanel.java
@@ -0,0 +1,37 @@
+package com.anthony.fernandez.nightly.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.anthony.fernandez.nightly.R;
+
+public class LeftPanel extends SherlockFragment {
+
+	private View v;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		v = (ViewGroup) inflater.inflate(R.layout.fragment_left, container, false);
+		
+		return v;
+	}
+
+	@Override
+	public void setUserVisibleHint(boolean isVisibleToUser) {
+		if(null != getActivity()){
+			getActivity().findViewById(R.id.selectorCurrent).setVisibility(View.VISIBLE);
+			getActivity().findViewById(R.id.selectorCurrentAlarm).setVisibility(View.GONE);
+			((TextView)getActivity().findViewById(R.id.moonText)).setTextColor(getResources().getColor(R.color.blue_background_dark));
+			((TextView)getActivity().findViewById(R.id.alarmText)).setTextColor(getResources().getColor(R.color.unpress_grey));
+			((ImageView)getActivity().findViewById(R.id.alarmIcon)).setImageResource(R.drawable.alarm);
+			((ImageView)getActivity().findViewById(R.id.moonIcon)).setImageResource(R.drawable.moon);
+		}
+		super.setUserVisibleHint(isVisibleToUser);
+	}
+}

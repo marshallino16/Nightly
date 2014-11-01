f38cc2e8d973e7d1fe1a7ba640f0e36d526ffb85
Genyus <anthonyfernandez16@gmail.com>
2014-11-01 12:36:08 +0100

    add tuoch event on circle, change left panel picto

--- src/com/anthony/fernandez/nightly/MainActivity.java
+++ src/com/anthony/fernandez/nightly/MainActivity.java
@@ -3,16 +3,21 @@ package com.anthony.fernandez.nightly;
 import java.util.List;
 import java.util.Vector;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.anthony.fernandez.nightly.adapter.InitPagerAdapter;
 import com.anthony.fernandez.nightly.fragment.LeftPanel;
 import com.anthony.fernandez.nightly.fragment.RightPanel;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.viewpagerindicator.CirclePageIndicator;
 
 public class MainActivity extends SherlockFragmentActivity {
@@ -25,10 +30,20 @@ public class MainActivity extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		//Project ID: animated-codex-750 Project Number: 926014171825
+		//API KEY = AIzaSyBod3FoppvHG6F8lXihLLwg433pJnkmscQ
 		getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 		getSupportActionBar().setCustomView(R.layout.action_bar_menu);
 		getSupportActionBar().setHomeButtonEnabled(true);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			setTranslucentStatus(true); 
+		} 
+		
+		SystemBarTintManager tintManager = new SystemBarTintManager(this);
+	    tintManager.setStatusBarTintEnabled(true);
+	    tintManager.setNavigationBarTintEnabled(true);
+	    tintManager.setTintColor(getResources().getColor(R.color.blue_aciton_bar));
 
 		fragments = new Vector<Object>();
 
@@ -47,4 +62,17 @@ public class MainActivity extends SherlockFragmentActivity {
         indicator.setStrokeColor(getResources().getColor(android.R.color.white));
 		indicator.setViewPager(pager);
 	}
+	
+	@TargetApi(19)  
+	private void setTranslucentStatus(boolean on) {
+		Window win = getWindow();
+		WindowManager.LayoutParams winParams = win.getAttributes();
+		final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+		if (on) {
+			winParams.flags |= bits;
+		} else { 
+			winParams.flags &= ~bits;
+		} 
+		win.setAttributes(winParams);
+	} 
 }
